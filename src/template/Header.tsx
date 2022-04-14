0118c22a06926985402765cf1ebb3dcab67b82d2
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-04-14 15:35:27 +0700

    update logout
    update logout

--- src/template/Header.tsx
+++ src/template/Header.tsx
@@ -21,6 +21,7 @@ import {
 import { RootState } from './../store/ConfigureStore'
 import { useSelector, useDispatch } from 'react-redux'
 import { setLoginfail, deleteUser } from './../store/reducer/User'
+import exportedSwal from '../utils/swal';
 //import exportedSwal from '../utils/swal';
 
 const lightColor = 'rgba(255, 255, 255, 0.7)';
@@ -42,8 +43,8 @@ export default function Header(props: HeaderProps) {
     dispatch(setLoginfail())
     dispatch(deleteUser())
     
-    //exportedSwal.actionSuccess("ทำการออกจากระบบเรียบร้อย กรุณารอสักครู่ ระบบจะพาคุณไปยังหน้า Login")
-    window.location.href = `https://mis-ird.rmuti.ac.th/sso/auth/logout?url=https://mis-ird.rmuti.ac.th/gs/admin/`
+    exportedSwal.actionSuccess("ทำการออกจากระบบเรียบร้อย กรุณารอสักครู่ ระบบจะพาคุณไปยังหน้า Login")
+    //window.location.href = `https://mis-ird.rmuti.ac.th/sso/auth/logout?url=https://mis-ird.rmuti.ac.th/gs/admin/`
   }
 
   return (
